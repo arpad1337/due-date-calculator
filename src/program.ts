9f75cfc45f03662e91cde209c4ca74db3152b86b
Kiss <arpad1337@me.com>
2019-08-26 21:36:57 +0200

    Removed timezone

--- src/program.ts
+++ src/program.ts
@@ -7,7 +7,7 @@ import { DateUtils } from './date-utils';
 export class Program {
     static main(argv: string[]): void {
         if (argv.length < 2) {
-            throw new Error("You must supply submissionDate && turnaround hours as arguments, eg: \"2019-10-18T16:02:00Z\" \"10\"");
+            throw new Error("You must supply submissionDate && turnaround hours as arguments, eg: \"2019-10-18T16:02:00\" \"10\"");
         }
         const submissionDate = new Date(argv[0]);
         const turnaroundHours = Math.abs(parseInt(argv[1], 10)); 
